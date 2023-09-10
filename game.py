c8b8c1bcd39df8ae75a09d2253eb50759a094885
Risik34 <risimn7@gmail.com>
2023-09-10 13:44:54 +0000

    Initial commit

---
+++ game.py
@@ -0,0 +1,27 @@
+import os
+import time
+
+k="⬜"
+board=[
+  [k,k,k],
+  [k,k,k],
+  [k,k,k]
+]
+
+
+def printer():
+  #os.system("clear")
+  print("\n\n")
+  
+  for i in range(3):
+    print("\t\t\t\t",end="")
+    
+    for k in board[i]:
+      print(f"{k}\t",end="")
+    print ("\n")
+
+# PLAYER SYMBOLS ADDER
+
+def addx(row, column):
+  board[int(row)-1][int(column)-1]="❌"
+  
---
+++ main.py
@@ -0,0 +1,6 @@
+import game 
+import time
+game.printer()
+
+game.addx(3,3)
+game.printer()
